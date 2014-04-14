e66f5cb447b189202b128b7f4dd9742cb919ab14
rmjoia <ricardo.joia@gmail.com>
2014-04-14 10:05:37 +0100

    FileImport Creation
    plain regular import form

---
+++ FileImport.php
@@ -0,0 +1,9 @@
+<html>
+	<body>
+		<form method="post" enctype="multipart/form-data" action="processform.php">
+		<input type="hidden" name="MAX_FILE_SIZE" value="300000" />
+		<input type="thefile" /><br/>
+		<input type="submit" value="Import PPTX" />
+		</form>
+	</body>
+</html>
